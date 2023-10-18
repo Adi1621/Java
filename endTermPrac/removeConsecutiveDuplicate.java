1aad092b14aa37b3f4da0657a6e44aa686bb750b
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-18 15:05:17 +0530

    removing duplicate element from an array

---
+++ endTermPrac/removeConsecutiveDuplicate.java
@@ -0,0 +1,27 @@
+package endTermPrac;
+
+import java.util.*;
+
+public class removeConsecutiveDuplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ArrayList<Integer> brr = new ArrayList<>();
+        brr.add(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                brr.add(arr[i]);
+            } else {
+                continue;
+            }
+        }
+        System.out.println(brr);
+
+    }
+
+}

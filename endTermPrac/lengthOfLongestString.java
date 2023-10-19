2e58175435132687aeeb0b2314ac56068f4b96e4
Aditya Kanswal <96226869+Adi1621@users.noreply.github.com>
2023-10-19 11:00:20 +0530

    lengthOfLongestString.java

--- endTermPrac/lengthOfLongestString.java
+++ endTermPrac/lengthOfLongestString.java
@@ -1,13 +1,28 @@
 package endTermPrac;
+
 import java.util.*;
+
 public class lengthOfLongestString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        
-        
+        String[] words = str.split(" ");
+        String longest = "";
 
+        int maxLen = 0;
+        String ans = "";
+        for (int i = 0; i < words.length; i++) {
+            String sample = words[i];
+            int len = sample.length();
+            if (len > maxLen) {
+                maxLen = len;
+                ans = "";
+                ans += sample;
+            }
+        }
+        System.out.println(maxLen);
+        System.out.println(ans);
 
     }
-    
+
 }

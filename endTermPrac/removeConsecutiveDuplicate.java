25029212ef8b9e303477efc4e8c0b8951cf593c3
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-20 22:21:51 +0530

    Removing Consecutive Duplicate Element

--- endTermPrac/removeConsecutiveDuplicate.java
+++ endTermPrac/removeConsecutiveDuplicate.java
@@ -6,6 +6,7 @@ public class removeConsecutiveDuplicate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();

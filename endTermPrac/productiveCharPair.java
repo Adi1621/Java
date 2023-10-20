38540852286464f8d6da5267b36187b812e8ff9e
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-20 13:31:30 +0530

    Removing Smaller Character From Pair Of Two On Comparing Lexpgraphically

---
+++ endTermPrac/productiveCharPair.java
@@ -0,0 +1,31 @@
+package endTermPrac;
+
+import java.util.*;
+
+public class productiveCharPair {
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        int j = i + 1;
+        while (i < str.length() && j < str.length()) {
+            if (str.charAt(i) > str.charAt(j)) {
+                sb.append(str.charAt(i));
+            } else {
+                sb.append(str.charAt(j));
+            }
+            i = i + 2;
+            j = i + 1;
+        }
+
+        if (str.length() % 2 != 0) {
+            sb.append(str.charAt(i));
+        }
+
+        System.out.println(sb);
+    }
+
+}

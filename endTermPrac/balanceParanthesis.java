2b18883148ee5bd657a28a288db506cb903a26ea
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-20 22:19:17 +0530

    Balance Paranthesis

--- endTermPrac/balanceParanthesis.java
+++ endTermPrac/balanceParanthesis.java
@@ -6,8 +6,10 @@ public class balanceParanthesis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        
         int i = 0;
         Stack<Character> stack = new Stack<>();
+
         while (i < str.length()) {
             if (str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '<') {
                 stack.push(str.charAt(i));

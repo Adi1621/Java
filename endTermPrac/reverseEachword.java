fd5486f4b06379347e1c3229cac425651ad664fe
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-21 20:47:09 +0530

    Reversing Each Letter Of Word In Sentence

--- endTermPrac/reverseEachword.java
+++ endTermPrac/reverseEachword.java
@@ -6,18 +6,18 @@ public class reverseEachword {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+
         String[] words = str.split(" ");
 
-        // for storing reverse of a string
         String revString = "";
 
-        
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
             String revWord = "";
             for (int j = word.length() - 1; j >= 0; j--) {
                 revWord = revWord + word.charAt(j);
             }
+
             revString = revString + revWord + " ";
 
         }

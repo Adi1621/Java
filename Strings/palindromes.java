a101c5318574dc48ac4e5bf46352ae8437839058
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-27 23:27:35 +0530

    Palindromes

--- Strings/palindromes.java
+++ Strings/palindromes.java
@@ -4,6 +4,7 @@ public class palindromes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+
         Boolean a = false;
         for (int i = 0, j = s.length()-1 ; i < s.length()/2 && j >= s.length()/2; i++, j--) {
             if (s.charAt(i) == s.charAt(j)) {
@@ -13,6 +14,7 @@ public class palindromes {
              a = false;
             }
         }
+        
         System.out.print(a);
     }
 

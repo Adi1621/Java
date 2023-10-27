24084bd4f8e173c41d80dcbe99717061ce9e2de5
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-27 23:26:36 +0530

    Array Input

--- arrayInput.java
+++ arrayInput.java
@@ -2,13 +2,16 @@ import java.util.*;
 public class arrayInput {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter size of array");
         int n = sc.nextInt();
         int[] arr = new int[n];
+
         System.out.println("Enter " + n + " elements");
         for(int i =0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+        
         for(int i = 0;i<arr.length;i++){
             System.out.println(arr[i]);
         }

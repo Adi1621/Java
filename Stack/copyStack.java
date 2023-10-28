3a371f751729642e623fccfb0349db9da2be22a5
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-28 23:48:47 +0530

    Copy Stack

--- Stack/copyStack.java
+++ Stack/copyStack.java
@@ -4,10 +4,11 @@ import java.util.*;
 public class copyStack {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         Stack<Integer> stack = new Stack<>();
         int n;
         n=sc.nextInt();
-        
+
         for(int i = 1;i<=n;i++){
             int x = sc.nextInt();
             stack.push(x);

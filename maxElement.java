01182a922604cac8ea4e84969bd07abae33fc56f
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-29 23:27:23 +0530

    maxElement

--- maxElement.java
+++ maxElement.java
@@ -2,6 +2,7 @@ public class maxElement {
     public static void main(String[] args){
         int arr[] = {4, 6, 3, 10};
         int max = 0;
+        
         for( int i=0;i< arr.length;i++){
             if(arr[i]>max){
                 max = arr[i];

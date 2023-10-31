449c199b1893dbadd5a7ad9bef0c9bd560562d17
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-31 14:16:09 +0530

    Array Question

---
+++ checkSorted.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class checkSorted {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int flag = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
+                flag = flag + 1;
+
+            } else {
+                System.out.println("Not Sorted");
+            }
+        }
+        if (flag == arr.length - 1) {
+            System.out.println("Sorted");
+        }
+    }
+
+}

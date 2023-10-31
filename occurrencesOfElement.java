42e15b5aff94db95aa2645b925a017bfea2b74b8
unknown <aditya1621.be21@chitkara.edu.in>
2023-10-31 10:48:51 +0530

    Updated and Formatted Code

--- occurrencesOfElement.java
+++ occurrencesOfElement.java
@@ -1,27 +1,28 @@
 import java.util.*;
+
 public class occurrencesOfElement {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter size of array");
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.println("Enter " + n + " elements");
-        for(int i =0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter element");
-            int x = sc.nextInt();
-            int count = 0;
-            for(int i = 0;i<arr.length;i++){
-                if(arr[i] == x){
+        int x = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
                 count++;
-                 
-            }
-            
-            }
-            System.out.println(count);
-            
-    }
 
+            }
+
+        }
+        System.out.println(count);
+
+    }
 
 }

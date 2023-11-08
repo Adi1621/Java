88cf57c5048af9dc68ac8f3f199f4e0021dbf941
unknown <aditya1621.be21@chitkara.edu.in>
2023-11-08 14:24:20 +0530

    Linear Search

--- SearchingAlgorithms/linearSeach.java
+++ SearchingAlgorithms/linearSeach.java
@@ -1,15 +1,26 @@
 package SearchingAlgorithms;
 
+import java.util.*;
+
 public class linearSeach {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[] arr = { 1, 4, 6, 7, 2 };
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
 
-        int key = 2; // return the index of 2
+        System.out.println("Enter " + n + " elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.println("Enter the value of key");
+        int key = sc.nextInt();
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 2) {
-                System.out.println(i);
+            if (arr[i] == key) {
+                System.out.println(key + " is present at " + i + "th " + " index");
             }
 
         }

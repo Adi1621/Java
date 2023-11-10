dbcb99c5696f7225cdf71f63245521c07505a343
unknown <aditya1621.be21@chitkara.edu.in>
2023-11-10 23:22:49 +0530

    Binary Search

--- SearchingAlgorithms/binarySearch.java
+++ SearchingAlgorithms/binarySearch.java
@@ -4,14 +4,14 @@ package SearchingAlgorithms;
 public class binarySearch {
 	public static void main(String[] args) {
 		int arr[] = {1,3,4,5,6,7,8,9,10,11,13,15,16};
-		int target = 9;
+		int target = 15;
 		int start =0;
 		int end=arr.length-1;
 		int mid = start + end / 2;
 		while(start>end){
-			if(target==mid){
+			if(target==mid{
 				System.out.println(arr[mid]);
-			}
+			})
 
 		}
 	}

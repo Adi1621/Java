baf8291c612c9c7f30d9afa0bbe824ed43f4e134
unknown <aditya1621.be21@chitkara.edu.in>
2023-12-08 23:48:51 +0530

    Find Pattern

--- Strings/findPattern.java
+++ Strings/findPattern.java
@@ -1,3 +1,26 @@
 public class findPattern {
-    
+    public static void main(String[] agrs) {
+        String str = "AAABBBCCCDDDEEE";
+        String pattern = "BBBCCC";
+        int flag = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String sub = str.substring(i, j);
+                if (sub.equals(pattern)) {
+                    System.out.println(i);
+                    flag = 1;
+                    break;
+
+                }
+
+            }
+        }
+
+        if (flag == 0) {
+            System.out.println("not found");
+        }
+
+    }
+
 }

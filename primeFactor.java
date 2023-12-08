0115405635624fd2dfd334ab8e2d18f2c11a4cf8
unknown <aditya1621.be21@chitkara.edu.in>
2023-12-09 00:37:04 +0530

    Program To Find Prime Factor In Java

--- primeFactor.java
+++ primeFactor.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 public class primeFactor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         ArrayList<Integer> ans = new ArrayList<>();
 
-        for(int i = 2; i<=n;i++){
-            while(n%i==0){
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
                 n = n / i;
                 ans.add(i);
 
@@ -15,5 +16,5 @@ public class primeFactor {
 
         System.out.println(ans);
     }
-    
+
 }

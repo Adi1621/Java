536ad7a43b646b8c2f51c7494075f154505cacf9
unknown <aditya1621.be21@chitkara.edu.in>
2023-12-09 00:35:42 +0530

    Find prime factors

--- primeFactor.java
+++ primeFactor.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class primeFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for(int i = 2; i<=n;i++){
+            while(n%i==0){
+                n = n / i;
+                ans.add(i);
+
+            }
+        }
+
+        System.out.println(ans);
+    }
     
 }

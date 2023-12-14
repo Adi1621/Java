8cf1220454c4239e7608bccd84f0269ae3bcc794
unknown <aditya1621.be21@chitkara.edu.in>
2023-12-14 09:53:32 +0530

    Finding nth Fibonacci Number Using Recursion

---
+++ Recursion/nthFibonacciNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class nthFibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fibo(n));
+    }
+
+    static int fibo(int n){
+        //base case
+        if(n<2){
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }    
+}

15438c31b3020f863c5d8f29b2bfb6c334e90d2b
unknown <aditya1621.be21@chitkara.edu.in>
2024-02-26 23:54:56 +0530

    Merge Sort

---
+++ Recursion/mergeSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class mergeSort {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] rigth = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, rigth);
+    }
+
+    static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < first.length && j < second.length){
+            if(first[i] < second[j]){
+            }
+        }
+    }
+}
